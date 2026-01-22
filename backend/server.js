88ed3431b857edd3de4bacd837bc91f2a6e146ba
ike-agu <ikennaww@yahoo.fr>
2026-01-22 15:01:16 +0000

    feat:Added Post end point so a  user can send chat messages

--- backend/server.js
+++ backend/server.js
@@ -7,6 +7,7 @@ const port = 3000;
 app.use(cors());
 app.use(express.json())
 
+// in memory array for chat list
 const chatList = [
   {
     name: "Ike",
@@ -35,6 +36,31 @@ app.get("/chat",(req, res) =>{
   const chat = displayChat();
   res.json(chat);
 })
+
+// ==Post method ==
+app.post("/chat", (req, res) => {
+  const body = req.body;
+  // add validations
+  if(!body || typeof body !== "object" || !body.name || !body.text){
+    return res.status(400).json({error:"Expected body to be Json Object"});
+  }
+
+  const name = String(body.name || "").trim();
+  const text = String(body.text || "").trim();
+
+  // validate name and text
+  if(!name){
+    return res.status(400).json({error: "Name cannot be empty"})
+  }
+
+  if(!text){
+    return res.status(400).json({error:"Text cannot be empty"});
+  }
+
+  chatList.push({name, text});
+  return res.status(201).json({status:"OK", saved: {name, text}});
+})
+
 
 app.listen(port, () => {
   console.log(`server listening on port: ${port}`);
