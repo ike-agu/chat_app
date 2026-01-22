2890bf9d0c972be0f6797fe9bc41b45352235a0f
ike-agu <ikennaww@yahoo.fr>
2026-01-22 14:21:02 +0000

    feat: add GET /chat endpoint with seeded messages

--- backend/server.js
+++ backend/server.js
@@ -5,10 +5,36 @@ const app = express();
 const port = 3000;
 
 app.use(cors());
+app.use(express.json())
+
+const chatList = [
+  {
+    name: "Ike",
+    text: "Hello there",
+  },
+  {
+    name: "Maria",
+    text: "Hi there",
+  }
+];
+
+function displayChat(){
+  return chatList.map((item) => ({
+    name: item.name,
+    text: item.text
+  }))
+}
 
 app.get("/", (req, res) => {
   res.send("Hello, from the server!");
 });
+
+// ===Get request====
+app.get("/chat",(req, res) =>{
+  console.error("Received a request from a chat")
+  const chat = displayChat();
+  res.json(chat);
+})
 
 app.listen(port, () => {
   console.log(`server listening on port: ${port}`);
